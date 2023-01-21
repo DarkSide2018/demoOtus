df530dc578e920d46ca8d6f402ee7f7535216291
Roman Gaponov <roman.gaponov@agilepartners.eu>
2023-01-21 17:05:10 +0300

    added examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,40 @@
-class Main {
+
+
+fun main() {
+    val triangle = Triangle(4, 5, 6)
+    val quadrAngle = QuadrAngle(4, 5)
+    listOf<Figure>(triangle,quadrAngle).forEach {
+        it.print()
+    }
 
 }
-fun main(){
-    println("hello world")
+abstract class Figure(
+    val height: Int,
+    val width: Int
+){
+    abstract fun print()
+}
+class QuadrAngle(
+    height: Int,
+    width: Int,
+):Figure(
+    height=height,
+    width=width
+){
+    override fun print(){
+        println("width=$width height=$height")
+    }
+}
+ class Triangle(
+     val diagonal: Int,
+     height: Int,
+     width: Int,
+) : Figure(
+    height=height,
+    width=width
+){
+    override fun print(){
+        println("diagonal=$diagonal width=$width height=$height")
+    }
+
 }
